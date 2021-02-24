df72c9248d1697812f378018778ee4b1211566e2
Straight <gengzhi1993@163.com>
2021-02-24 13:11:28 +0800

    创建 plugin.js

--- test/input.test.js
+++ test/input.test.js
@@ -72,7 +72,7 @@ describe('Input', () => {
 				let inputElement = vm.$el.querySelector('input')
 				inputElement.dispatchEvent(event)
 				expect(callback).to.have.been.calledWith('hi')
-				
+
 			})
 		})
 	})
