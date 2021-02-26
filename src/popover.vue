eb372c55428c45077f640462b0f9ff873e472c47
Straight <gengzhi1993@163.com>
2021-02-26 20:46:52 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref = "contentWrapper" class = "contentWrapper" v-if = "visible">
       <slot name = "content"></slot>
     </div>
-    <div ref = "triggerWrapper">
+    <div ref = "triggerWrapper" style = "display: inline-block">
       <slot></slot>
     </div>
   </div>
@@ -26,10 +26,11 @@ export default {
     },
     onClickDocument(e) {
       if (this.$refs.popover &&
-        (this.$refs.popover === e.target
-          || this.$refs.popover.contains(e.target)
-          || this.$refs.contentWrapper === e.target)
+        (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
       ) {return }
+      if (this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+      ) { return}
       this.close()
     },
     open() {
@@ -57,14 +58,41 @@ export default {
 </script>
 
 <style lang = "scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   position: relative;
   vertical-align: top;
 }
 .contentWrapper {
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
   position: absolute;
   transform: translateY(-100%);
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  //box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background: white;
+  padding: 0.5em 1em;
+  margin-top: -10px;
+  max-width: 20em;
+  word-break: break-all;
+  &::before, &::after {
+    content: '';
+    display: block;
+    width: 0;
+    height: 0;
+    position: absolute;
+    border: 10px solid transparent;
+    left: 10px;
+  }
+  &::before {
+    top: 100%;
+    border-top-color: black;
+  }
+  &::after {
+    top: calc(100% - 1px);
+    border-top-color: white;
+  }
 }
 </style>
