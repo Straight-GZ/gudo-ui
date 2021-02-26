b95b131d3cd2122f4d58b07a2e95dcbc21b53553
Straight <gengzhi1993@163.com>
2021-02-26 14:13:02 +0800

    实现 点击弹出

---
+++ src/popover.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class = "popover">
+    <div class = "contentWrapper">
+      <slot name = "content" v-if = "visible"></slot>
+    </div>
+    <div @click = "xxx">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JianPopover',
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+
+<style lang = "scss" scoped>
+.popover {
+  display: inline-block;
+  position: relative;
+  > .contentWrapper {
+    position: absolute;
+    bottom: 100%
+  }
+}
+
+</style>
