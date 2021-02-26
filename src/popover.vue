a5bc024678fd57425ac2c38b7a38d2067c9865b3
Straight <gengzhi1993@163.com>
2021-02-26 16:05:35 +0800

    获取 trigger的位置，将content设置在上方

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,9 @@
     <div ref = "contentWrapper" class = "contentWrapper" v-if = "visible" @click.stop>
       <slot name = "content"></slot>
     </div>
-    <slot></slot>
+    <div ref = "triggerWrapper">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -19,12 +21,15 @@ export default {
     xxx() {
       this.visible = !this.visible
       if (this.visible === true) {
-        let eventHandler = () => {
-          this.visible = false
-          document.removeEventListener('click', eventHandler)
-        }
         this.$nextTick(() => {
           document.body.appendChild(this.$refs.contentWrapper)
+          let {left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+          this.$refs.contentWrapper.style.left = left + 'px'
+          this.$refs.contentWrapper.style.top = top + 'px'
+          let eventHandler = () => {
+            this.visible = false
+            document.removeEventListener('click', eventHandler)
+          }
           document.addEventListener('click', eventHandler)
         })
       }
@@ -38,12 +43,10 @@ export default {
   display: inline-block;
   position: relative;
   vertical-align: top;
-  > .contentWrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
 }
-
+.contentWrapper {
+  position: absolute;
+  transform: translateY(-100%);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+}
 </style>
